44bd207cd75078d35c6d2a5a2bef5aa5010c2ba1
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-07-02 15:41:18 -0700

    Add additional tests for Battleship shipFactory

--- src/battleship-game-logic.js
+++ src/battleship-game-logic.js
@@ -2,6 +2,34 @@ const sum = (a, b) => {
 	return a + b;
 };
 
+// Ship Factory Function
+const shipFactory = (shipLength) => {
+	let isSunk = false;
+
+	const hitArray = [];
+	for (let i = 0; i < shipLength; i++) {
+		hitArray.push(false);
+	}
+	const hit = (spotHit) => {
+		hitArray[spotHit] = true;
+	}
+
+	return { shipLength, isSunk, hitArray, hit }
+};
+
+// After each hit, check if it should be sunk.
+const shipIsSunk = (shipArray) => {
+	// check if hitArray is ALL true, is so, ship isSunk = true;
+	if (shipArray.includes(false)) {
+		return false;
+	} else { 
+		return true;
+	}
+};
+
+
 export {
-	sum
+	sum,
+	shipFactory,
+	shipIsSunk
 }
--- src/battleship-game-logic.test.js
+++ src/battleship-game-logic.test.js
@@ -1,5 +1,53 @@
 import { sum } from './battleship-game-logic';
+import { shipFactory } from './battleship-game-logic';
+import { shipIsSunk } from './battleship-game-logic';
 
 test('TEST FOR JEST: Adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('Creates a Ship Object with length of 5', () => {
+  const newShip = shipFactory(5);
+  expect(newShip.shipLength).toBe(5);
+});
+
+test('Creates a Ship Object with length of 3', () => {
+  const newShip = shipFactory(3);
+  expect(newShip.shipLength).toBe(3);
+});
+
+test('Hits a ship, i.e. sets hitArray[i] to True', () => {
+  const newShip = shipFactory(3);
+  newShip.hit(3);
+  expect(newShip.hitArray[3]).toBe(true);
+});
+
+test('Hits a ship, i.e. sets hitArray[i] to True (Length of 1)', () => {
+  const newShip = shipFactory(1);
+  newShip.hit(1);
+  expect(newShip.hitArray[1]).toBe(true);
+});
+
+test('Sinks a Ship (length 1)', () => {
+  const newShip = shipFactory(1);
+  newShip.hit(0);
+  newShip.isSunk = shipIsSunk(newShip.hitArray);
+
+  expect(newShip.isSunk).toBe(true);
+});
+
+test('Sinks a Ship (length 5)', () => {
+  const newShip = shipFactory(5);
+  for (let i = 0; i < newShip.shipLength; i++) {
+    newShip.hit(i);  
+  }
+
+  newShip.isSunk = shipIsSunk(newShip.hitArray);
+
+  expect(newShip.isSunk).toBe(true);
+});
+
+// test('Creates a Ship Object with length of 5', () => {
+//   const newShip = shipFactory(5);
+//   expect(newShip.shipLength).toBe(5);
+// });
